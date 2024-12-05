efc60ab600b8beba75a87ceee5c4547a925fa128
Grigori Gratchev <ggratchev@iutbayonne.univ-pau.fr>
2024-12-05 08:41:43 +0100

    modif du main pour spécifier explicitement le working directory

--- EcoflocConfigurator/App.xaml.cs
+++ EcoflocConfigurator/App.xaml.cs
@@ -1,14 +1,23 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using System;
+using System.IO;
 using System.Windows;
 
 namespace EcoflocConfigurator
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
 
+            // Afficher le répertoire de travail pour le débogage
+            //Console.WriteLine("Répertoire de travail initial : " + Directory.GetCurrentDirectory());
+
+            // Changer explicitement le répertoire de travail
+            string currentDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            Directory.SetCurrentDirectory(currentDirectory);
+
+            //Console.WriteLine("Répertoire de travail défini : " + Directory.GetCurrentDirectory());
+        }
+    }
 }
