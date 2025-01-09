5c4f12da4f114b6523688bc66f552e004aea3ca2
Mattis Pingard <mattis.pingard@gmail.com>
2025-01-09 08:54:49 +0100

    [~] Correct to meet PR request from reviewers

--- ecofloc4win/ecofloc4win.cpp
+++ ecofloc4win/ecofloc4win.cpp
@@ -28,8 +28,6 @@
 #include <iostream>
 #include <tcpmib.h>
 
-#pragma comment(lib, "Ws2_32.lib") // Link with Ws2_32.lib
-
 #include "process.h"         // Custom header for process handling
 #include "gpu.h"             // Custom header for GPU monitoring
 #include "MonitoringData.h"  // Custom header for monitoring data
@@ -422,7 +420,7 @@ int main()
                             continue;
                         }
 
-                        process_counters[instanceName] = { counterDiskRead, counterDiskWrite };
+                           process_counters[instanceName] = { counterDiskRead, counterDiskWrite };
                     }
                 }
             }
@@ -476,16 +474,16 @@ int main()
                 PMIB_TCPTABLE_OWNER_PID tcpTable = nullptr;
                 ULONG ulSize = 0;
 
+				// Get the size of the table
                 if (GetExtendedTcpTable(nullptr, &ulSize, TRUE, AF_INET, TCP_TABLE_OWNER_PID_ALL, 0) != ERROR_INSUFFICIENT_BUFFER) {
-                    //std::cerr << "Failed to determine size of TCP table." << std::endl;
                     continue;
                 }
 
                 std::unique_ptr<BYTE[]> buffer(new BYTE[ulSize]);
                 tcpTable = reinterpret_cast<PMIB_TCPTABLE_OWNER_PID>(buffer.get());
 
+				// Get the table data
                 if (GetExtendedTcpTable(tcpTable, &ulSize, TRUE, AF_INET, TCP_TABLE_OWNER_PID_ALL, 0) != NO_ERROR) {
-                    //std::cerr << "Failed to retrieve TCP table." << std::endl;
                     continue;
                 }
 
@@ -499,32 +497,33 @@ int main()
 
                         if (SetPerTcpConnectionEStats(reinterpret_cast<PMIB_TCPROW>(&row), TcpConnectionEstatsData,
                             reinterpret_cast<PUCHAR>(&rwData), 0, sizeof(rwData), 0) != NO_ERROR) {
-                            //std::cerr << "Failed to enable ESTATS for PID: " << data.getPids()[0] << std::endl;
                             continue;
                         }
-
-
 
                         if (row.dwState == MIB_TCP_STATE_ESTAB && row.dwRemoteAddr != htonl(INADDR_LOOPBACK)) {
                             ULONG rodSize = sizeof(TCP_ESTATS_DATA_ROD_v0);
                             std::vector<BYTE> rodBuffer(rodSize);
                             PTCP_ESTATS_DATA_ROD_v0 dataRod = reinterpret_cast<PTCP_ESTATS_DATA_ROD_v0>(rodBuffer.data());
 
+							// Get the ESTATS data for this connection
                             if (GetPerTcpConnectionEStats(reinterpret_cast<PMIB_TCPROW>(&row), TcpConnectionEstatsData,
                                 nullptr, 0, 0, nullptr, 0, 0,
                                 reinterpret_cast<PUCHAR>(dataRod), 0, rodSize) == NO_ERROR) {
+
+								std::cout << "DataBytesIn: " << dataRod->DataBytesIn << std::endl;
+								std::cout << "DataBytesOut: " << dataRod->DataBytesOut << std::endl;
 
                                 // Calculate Bytes In and Bytes Out
                                 double bytesIn = static_cast<double>(dataRod->DataBytesIn);
                                 double bytesOut = static_cast<double>(dataRod->DataBytesOut);
 
-                                double intervalSec = 500.0 / 1000.0;
+								double intervalSec = 500.0 / 1000.0; // Interval in seconds (will be changed in the future)
 
 								long downloadRate = bytesIn / intervalSec;
 								long uploadRate = bytesOut / intervalSec;
 
-                                double downloadPower = 1.138 * ((double)downloadRate / 300000);
-                                double uploadPower = 1.138 * ((double)uploadRate / 300000);
+								double downloadPower = 1.138 * ((double)downloadRate / 300000); // 1.138 is the power consumption per byte for download (will be changed in the future using the config)
+								double uploadPower = 1.138 * ((double)uploadRate / 300000); // 1.138 is the power consumption per byte for upload (will be changed in the future using the config)
 
 								double averagePower = downloadPower + uploadPower;
 
@@ -546,7 +545,7 @@ int main()
             }
 
             screen.Post(Event::Custom);
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            std::this_thread::sleep_for(std::chrono::milliseconds(500)); // interval based on user input (will be changed in the future)
         }
         });
 
